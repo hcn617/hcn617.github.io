ff2fdf6eacd77ef7fcf7d0e00e10c76346af363a
hcn617 <hyouchun617@gmail.com>
2023-07-05 00:25:18 +0900

    スマホのとき、サブタイトルを削除する(javascript)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+// スマホとかで横幅がせまいとき、サブタイトルを削除する
+const width = window.screen.width;
+if (width < 700) {
+    const right_elements = document.getElementsByClassName("right_element");
+    for (element of right_elements) {
+        element.remove();
+    }
+}
