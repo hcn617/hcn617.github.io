8c6a09bdbfeae8116f0a208284d7b8712b824032
hcn617 <hyouchun617@gmail.com>
2023-07-19 02:56:44 +0900

    PythonEDの優先度早見表をつくったよ

--- js/main.js
+++ js/main.js
@@ -31,3 +31,86 @@ const indicate_time = () => {
     time.textContent = `${month}/${date} (${day}) ${hour}:${minute}:${second}`;
 }
 setInterval(indicate_time, 1000);
+
+
+// PythonEDのテーブルを作成
+const PythonED_table = document.getElementById("PythonED_table");
+const title = [
+    "Pythonの環境",
+    "コーディング規約",
+    "Pythonの言語仕様",
+    "Pythonのクラス",
+    "型ヒント",
+    "テキストの処理",
+    "数値の処理",
+    "日付と時刻の処理",
+    "データ型とアルゴリズム",
+    "汎用OS・ランタイムサービス",
+    "ファイルとディレクトリへのアクセス",
+    "データ圧縮、アーカイブと永続化",
+    "特定のデータフォーマットを扱う",
+    "インターネット上のデータを扱う",
+    "HTML/XMLを扱う",
+    "テスト",
+    "デバッグ",
+    "暗号関連",
+    "並行処理、並列処理"
+];
+const page = [
+    17 - 2,
+    39 - 18,
+    81 - 40,
+    107 - 82,
+    114 - 108,
+    145 - 124,
+    161 - 146,
+    176 - 162,
+    221 - 184 - (3 + 2),
+    245 - 222,
+    265 - 246,
+    283 - 266,
+    296 - 284,
+    338 - 320 - 5,
+    365 - 344,
+    399 - 366,
+    423 - 400,
+    441 - 424,
+    472 - 442
+];
+const question = [1, 2, 7, 3, 2, 4, 0, 2, 5, 2, 2, 0, 2, 2, 0, 3, 2, 1, 0];
+for (let i = 0; i < 19; i++) {
+    let new_row = document.createElement("tr");
+
+    let col1 = document.createElement("td");
+    col1.textContent = i + 1;
+    new_row.append(col1);
+
+    let col2 = document.createElement("td");
+    col2.textContent = title[i];
+    new_row.append(col2);
+
+    let col3 = document.createElement("td");
+    col3.textContent = page[i];
+    new_row.append(col3);
+
+    let col4 = document.createElement("td");
+    col4.textContent = question[i];
+    new_row.append(col4);
+
+    let col5 = document.createElement("td");
+    let priority_value = Math.round(1000 * question[i] / page[i]);
+    col5.textContent = priority_value;
+    new_row.append(col5);
+
+    if (priority_value == 0) {
+        new_row.style.backgroundColor = "silver";
+    } else if (priority_value <= 100) {
+        new_row.style.backgroundColor = "#66CC99";
+    } else if (priority_value <= 150) {
+        new_row.style.backgroundColor = "#FFFF44";
+    } else {
+        new_row.style.backgroundColor = "orange";
+    }
+
+    PythonED_table.append(new_row);
+}
